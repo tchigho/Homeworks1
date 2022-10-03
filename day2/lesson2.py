e60809decc78d9d1c16cf819949cdd0aa4d0eddf
tchigho <112646162+tchigho@users.noreply.github.com>
2022-10-03 16:42:30 -0700

    Update lesson2.py

---
+++ day2/lesson2.py
@@ -0,0 +1,9 @@
+name = "giorgi"
+username = "tchighitashvili"
+age = "15"
+height = "183"
+weight = "56"
+city = "Tbilisi"
+country = "saqartvelo"
+
+print("chemi saxeli da gvaria "+  name +" "+ username + " var " + age + " wlis simagle "+ height +"sm"  " wona ki " + weight +"kg" " vcxovrob "+ country +"-shi" " qalaq "+ city +"-shi")
